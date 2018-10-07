96a5eca68f87e847c5baee9998f7d8a8375a4cc6
benzsuankularb <benzsk130@gmail.com>
2018-10-07 08:29:09 +0700

    fixes

---
+++ EntitasJennyIssue/PositionComponent.cs
@@ -0,0 +1,16 @@
+﻿using Entitas;
+
+
+[Game]
+public sealed class PositionComponent : IComponent
+{
+
+	public float x;
+	public float y;
+
+	public override string ToString()
+	{
+		return "Position(" + x + ", " + y + ")";
+	}
+
+}
